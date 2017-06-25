10ad9ce4413bbe98ea08abbf2a84b85f661f36a4
Shishir Chawla <shishirchawla@gatech.edu>
2017-06-24 23:17:54 -0400

    Minor

--- src/quant.sh
+++ src/quant.sh
@@ -3,4 +3,7 @@
 rm -rf data
 mkdir data
 python main.py
-#&& HQuant -T 1 -C HQuant.conf -s 1 -n 1 256 -S trainlist.txt linvq
+
+# FIXME Remove the following line, this was used when the data was quantized for
+# the discrete model
+# HQuant -T 1 -C HQuant.conf -s 1 -n 1 256 -S trainlist.txt linvq
